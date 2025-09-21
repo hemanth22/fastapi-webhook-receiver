a15aff90954ce2cd3e14b3d6d5fc177d54a4aa82
Hemanth B <hemanth22hemu@gmail.com>
2025-09-21 21:27:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,6 +35,31 @@ CHAT_ID = os.environ.get('telegram_id')
 CHANNEL_CHAT_ID = '-1003097875450'
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+def gnewsstore(data):
+    formatted_message = (
+    f"Source: {data['source']}\n"
+    f"Title: {data['title']}\n"
+    f"Description: {data['description']}\n"
+    f"Url: {data['url']}\n"
+    f"Publish: {data['publishedTime']}\n"
+    f"SourceName: {data['sourcename']}"
+    )
+    payload_gnewsstore = {
+        'chat_id': CHANNEL_CHAT_ID,
+        'text': formatted_message,
+        'parse_mode': 'Markdown'  # Optional: Use 'HTML' if you prefer HTML formatting
+        }
+
+    # Send the message
+    response = requests.post(url, data=payload_gnewsstore)
+    # Check for successful response
+    if response.status_code == 200:
+        return "Message sent successfully."
+    else:
+        return f"Failed to send message. Status code: {response.status_code}"
+        return f"Response: {response.text}"
+
+
 def newsapistore(data):
     formatted_message = (
     f"Source: {data['source']}\n"
@@ -288,7 +313,7 @@ async def etfwebhook(request: Request):
         print("Webhook received (JSON):", payload)
         etfstore(payload)
     if content_type != "application/json":
-        print("Received Invalid Data", payload)
+        print("Received Invalid Dat", payload)
 
 @app.post("/mvetfwebhook")
 async def mvetfwebhook(request: Request):
@@ -307,5 +332,15 @@ async def newsapiwebhook(request: Request):
         payload = await request.json()
         print("Webhook received (JSON):", payload)
         newsapistore(payload)
+    if content_type != "application/json":
+        print("Received Invalid Data", payload)
+
+@app.post("/gnewswebhook")
+async def gnewswebhook(request: Request):
+    content_type = request.headers.get("Content-Type")
+    if content_type == "application/json":
+        payload = await request.json()
+        print("Webhook received (JSON):", payload)
+        gnewsstore(payload)
     if content_type != "application/json":
         print("Received Invalid Data", payload)
